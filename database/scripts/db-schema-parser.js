7fd417f9f4c0d74a58a83f5a981834b32a89bf1a
Damian Kowalski <dkowalski.1997@gmail.com>
2025-12-27 16:32:10 +0100

    added parser

---
+++ database/scripts/db-schema-parser.js
@@ -0,0 +1,23 @@
+const fs = require("fs").promises;
+const path = require("path");
+const logger = require("../../logger/logger");
+
+/**
+ * Parses the database schema in JSON format
+ */
+const parseSchema = async () => {
+  const file = await fs.readFile(path.join(__dirname, "../schema.json"));
+  const parsed = _parse(file);
+
+  console.log(parsed);
+};
+
+const _parse = (file) => {
+  try {
+    return JSON.parse(file);
+  } catch (err) {
+    logger.log("Could not parse JSON file - " + err.message);
+  }
+};
+
+module.exports = { parseSchema };
