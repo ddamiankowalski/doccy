58bb1bf8cdb7519e745f7ce197d73aa4fc3a29a0
Damian <dkowalski.1997@gmail.com>
2025-12-31 00:26:23 +0100

    created dashboard

--- apps/ui/src/app/app.routes.ts
+++ apps/ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './routes/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard,
+  },
+];
--- apps/ui/src/app/app.ts
+++ apps/ui/src/app/app.ts
@@ -1,10 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '../../node_modules/@angular/router/types/_router_module-chunk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
+  imports: [RouterOutlet],
 })
-export class App {
-  protected readonly title = signal('ui');
-}
+export class App {}
