7d4b893334a17929f3eec79a3e960474d9cb3e5d
Damian <dkowalski.1997@gmail.com>
2025-12-31 00:29:32 +0100

    import fix

--- apps/ui/src/app/routes/dashboard/dashboard.ts
+++ apps/ui/src/app/routes/dashboard/dashboard.ts
@@ -4,7 +4,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-dashboard',
   imports: [],
   template: `<p>dashboard works!</p>`,
-  styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {}
