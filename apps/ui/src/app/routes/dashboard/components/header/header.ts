c88b72d4372d763a7f87d38ca3475aef62096ed0
Damian Kowalski <dkowalski.1997@gmail.com>
2026-01-01 00:28:17 +0100

    added top menu

--- apps/ui/src/app/routes/dashboard/components/header/header.ts
+++ apps/ui/src/app/routes/dashboard/components/header/header.ts
@@ -8,7 +8,7 @@ import { LucideAngularModule } from 'lucide-angular';
     class: 'w-full',
   },
   imports: [HeaderUser, LucideAngularModule],
-  template: ` <header class="flex justify-between">
+  template: ` <header class="flex items-center justify-between">
     <div class="flex gap-6">
       <div class="flex justify-center items-center p-2 bg-white text-black rounded-lg w-12">
         <lucide-icon class="h-5 w-5" name="wallet-cards"></lucide-icon>
@@ -18,6 +18,11 @@ import { LucideAngularModule } from 'lucide-angular';
         <h2 class="text-gray-400 text-sm">Welcome back, here's your financial summary</h2>
       </div>
     </div>
+
+    <nav class="flex ml-auto mr-24 gap-8 items-center text-sm text-gray-500">
+      <button class="cursor-pointer hover:text-white transition-colors">Finance</button>
+      <button class="cursor-pointer hover:text-white transition-colors">Tasks</button>
+    </nav>
 
     <dc-header-user />
   </header>`,
