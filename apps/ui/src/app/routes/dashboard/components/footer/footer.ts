9a6c359dd087384e9334974e637dca56c4a035eb
Damian <dkowalski.1997@gmail.com>
2025-12-31 01:19:14 +0100

    wip

---
+++ apps/ui/src/app/routes/dashboard/components/footer/footer.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'dc-footer',
+  host: {
+    class:
+      'mt-20 pt-8 border-t border-white/5 flex flex-col md:flex-row justify-between items-center text-sm text-gray-500',
+  },
+  imports: [],
+  template: ` <section>©Copyright 2025 | Doccy</section>
+    <nav class="flex gap-6 mt-4 md:mt-0">
+      <button class="cursor-pointer hover:text-white transition-colors">Privacy Policy</button>
+      <button class="cursor-pointer hover:text-white transition-colors">Terms of Service</button>
+      <button class="cursor-pointer hover:text-white transition-colors">Help Center</button>
+    </nav>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer {}
--- apps/ui/src/app/routes/dashboard/dashboard.ts
+++ apps/ui/src/app/routes/dashboard/dashboard.ts
@@ -1,13 +1,19 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Header } from './components/header/header';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'dc-dashboard',
-  imports: [Header],
+  imports: [Header, RouterOutlet, Footer],
   host: {
     class: 'block p-12 min-h-screen text-white',
   },
-  template: ` <dc-header />`,
+  template: `
+    <dc-header />
+    <router-outlet />
+    <dc-footer />
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard {}
