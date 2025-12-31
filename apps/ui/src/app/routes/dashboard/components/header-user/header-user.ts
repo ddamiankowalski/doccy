41dcc3714a1292c4842257ac9bb516a3c45be15d
Damian <dkowalski.1997@gmail.com>
2025-12-31 01:11:10 +0100

    wip

--- apps/ui/src/app/routes/dashboard/components/header-user/header-user.ts
+++ apps/ui/src/app/routes/dashboard/components/header-user/header-user.ts
@@ -3,10 +3,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'dc-header-user',
   host: {
-    class: '',
+    class: 'flex gap-3 items-center',
   },
   imports: [],
-  template: ``,
+  template: `
+    <div class="flex flex-col items-end">
+      <span class="text-sm font-medium text-white">Alex Morgan</span>
+      <span class="text-xs text-gray-400">Standard User</span>
+    </div>
+
+    <div
+      class="h-10 w-10 rounded-full bg-gray-800 border border-gray-700 flex items-center justify-center overflow-hidden ring-2 ring-transparent hover:ring-gray-600 transition-all"
+    >
+      <img src="/img/profile.avif" />
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderUser {}
--- apps/ui/src/app/routes/dashboard/components/header/header.ts
+++ apps/ui/src/app/routes/dashboard/components/header/header.ts
@@ -9,7 +9,7 @@ import { HeaderUser } from '../header-user/header-user';
   imports: [HeaderUser],
   template: ` <header class="flex justify-between">
     <div class="flex gap-6">
-      <div class="p-2 bg-white text-black rounded-lg w-[3.25rem]"></div>
+      <div class="p-2 bg-white text-black rounded-lg w-12"></div>
       <div>
         <h1 class="text-2xl font-bold tracking-tight">Finance Overview</h1>
         <h2 class="text-gray-400 text-sm">Welcome back, here's your financial summary</h2>
