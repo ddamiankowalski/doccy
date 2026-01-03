00a3b001647cbc0700c8cede2bbd1d1656100549
Damian <dkowalski.1997@gmail.com>
2026-01-03 01:16:22 +0100

    added disclaimer

--- apps/ui/src/app/ui/components/disclaimer/disclaimer.ts
+++ apps/ui/src/app/ui/components/disclaimer/disclaimer.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'dc-disclaimer',
-  template: `i am disclaimer`,
+  imports: [LucideAngularModule],
+  host: {
+    class: 'text-center',
+  },
+  template: `
+    <div class="h-16 w-16 rounded-full bg-white/5 flex items-center justify-center mx-auto mb-4">
+      <lucide-icon class="h-5 w-5" [name]="icon()"></lucide-icon>
+    </div>
+
+    <h3 class="text-lg font-semibold text-white mb-2">{{ title() }}</h3>
+
+    <p class="text-gray-400 text-sm">{{ description() }}</p>
+  `,
 })
-export class Disclaimer {}
+export class Disclaimer {
+  public icon = input.required<string>();
+  public title = input.required<string>();
+  public description = input.required<string>();
+}
