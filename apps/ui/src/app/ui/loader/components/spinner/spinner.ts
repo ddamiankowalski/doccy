4e3fd3adb14b0570d6055171049cd4077b303385
Damian <dkowalski.1997@gmail.com>
2026-01-04 21:50:23 +0100

    added color

--- apps/ui/src/app/ui/loader/components/spinner/spinner.ts
+++ apps/ui/src/app/ui/loader/components/spinner/spinner.ts
@@ -4,6 +4,8 @@ import { LucideAngularModule } from 'lucide-angular';
 @Component({
   selector: 'dc-spinner',
   imports: [LucideAngularModule],
-  template: ` <lucide-icon class="h-5 w-5 animate-spin" name="loader-circle"></lucide-icon> `,
+  template: `
+    <lucide-icon class="text-white h-5 w-5 animate-spin" name="loader-circle"></lucide-icon>
+  `,
 })
 export class Spinner {}
