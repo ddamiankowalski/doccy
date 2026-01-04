a7902bc1ff9078b3062e3af77a9d07818636c79d
Damian <dkowalski.1997@gmail.com>
2026-01-04 21:53:14 +0100

    added spinner

--- apps/ui/src/app/routes/finance/components/finance-add/finance-add.ts
+++ apps/ui/src/app/routes/finance/components/finance-add/finance-add.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { InputText } from '../../../../ui/input/input-text/input-text';
+import { Spinner } from '../../../../ui/loader/components/spinner/spinner';
 
 @Component({
   selector: 'dc-finance-add',
   host: {
     class: 'min-w-[30rem]',
   },
-  imports: [InputText],
+  imports: [InputText, Spinner],
   template: `
+    @if(isLoading()) {
+    <dc-spinner class="p-4" />
+    } @else {
     <fieldset class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] max-w-[calc(2*1fr)]">
       <dc-input-text label="First name" placeholder="Enter first name" />
       <dc-input-text label="Last name" placeholder="Enter last name" />
       <dc-input-text label="Enter value" placeholder="Enter value" />
     </fieldset>
+    }
   `,
 })
-export class FinanceAdd {}
+export class FinanceAdd {
+  public isLoading = signal(true);
+
+  constructor() {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 2000);
+  }
+}
--- apps/ui/src/app/ui/loader/components/spinner/spinner.ts
+++ apps/ui/src/app/ui/loader/components/spinner/spinner.ts
@@ -4,8 +4,14 @@ import { LucideAngularModule } from 'lucide-angular';
 @Component({
   selector: 'dc-spinner',
   imports: [LucideAngularModule],
+  host: {
+    class: 'flex w-full h-full justify-center items-center',
+  },
   template: `
-    <lucide-icon class="text-white h-5 w-5 animate-spin" name="loader-circle"></lucide-icon>
+    <lucide-icon
+      class="text-white h-6 w-6 animate-spin [animation-duration:1.8s]"
+      name="loader-circle"
+    ></lucide-icon>
   `,
 })
 export class Spinner {}
