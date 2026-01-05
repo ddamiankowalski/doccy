9f37e5980b722a22497888ea5f22e7079d03f68b
Damian Kowalski <dkowalski.1997@gmail.com>
2026-01-05 22:11:20 +0100

    inputselect

--- apps/ui/src/app/ui/input/input-select/input-select.ts
+++ apps/ui/src/app/ui/input/input-select/input-select.ts
@@ -1,5 +1,16 @@
-import { Component, HostListener, input, model, signal } from '@angular/core';
+import {
+  Component,
+  DOCUMENT,
+  ElementRef,
+  HostListener,
+  inject,
+  input,
+  model,
+  signal,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormValueControl } from '@angular/forms/signals';
+import { filter, fromEvent } from 'rxjs';
 
 export type SelectOption = {
   value: string;
@@ -28,7 +39,7 @@ export type SelectOption = {
   },
   template: `
     <ul
-      class="absolute inset-x-0 top-full mt-2 px-3 py-2 flex flex-col gap-2 rounded-md bg-charcoal-light border border-white/50"
+      class="absolute inset-x-0 top-full mt-2 px-3 py-2 bg-charcoal-light flex flex-col gap-2 rounded-md border border-white/10"
       [class.hidden]="isOpen() === false"
     >
       @for(option of options(); track option.value) {
@@ -43,9 +54,27 @@ export class InputSelect implements FormValueControl<string> {
     this.isOpen.update((isOpen) => !isOpen);
   }
 
+  private _document = inject(DOCUMENT);
+  private _elementRef = inject(ElementRef);
+
   public value = model<string>('');
 
   public options = input<SelectOption[]>([]);
 
   public isOpen = signal<boolean>(false);
+
+  constructor() {
+    fromEvent(this._document.body, 'click')
+      .pipe(
+        takeUntilDestroyed(),
+        filter(({ target }) => {
+          const { nativeElement } = this._elementRef;
+          return !nativeElement.contains(target);
+        })
+      )
+      .subscribe((ev) => {
+        ev.stopPropagation();
+        this.isOpen.set(false);
+      });
+  }
 }
