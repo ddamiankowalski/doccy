fa26ee8109afec43950beac9412bf81261d531cf
Damian Kowalski <dkowalski.1997@gmail.com>
2026-01-06 13:00:30 +0100

    wip

--- apps/ui/src/app/routes/finance/store/finance-http.service.ts
+++ apps/ui/src/app/routes/finance/store/finance-http.service.ts
@@ -2,13 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+type FinanceResponse = {
+  sections: [];
+};
+
 @Injectable({
   providedIn: 'root',
 })
 export class FinanceHttpService {
   private _http = inject(HttpClient);
 
-  public fetch$(): Observable<any> {
-    return this._http.get('');
+  public fetch$(): Observable<FinanceResponse> {
+    return this._http.get<FinanceResponse>('api/finance');
   }
 }
