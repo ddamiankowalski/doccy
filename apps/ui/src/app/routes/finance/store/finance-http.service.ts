f1de3a34edb894e74000699c6aa49e729abbe287
Damian Kowalski <dkowalski.1997@gmail.com>
2026-01-06 12:51:35 +0100

    added finance http service

---
+++ apps/ui/src/app/routes/finance/store/finance-http.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FinanceHttpService {
+  private _http = inject(HttpClient);
+
+  public fetch$(): Observable<any> {
+    return this._http.get('');
+  }
+}
--- apps/ui/src/app/routes/finance/store/finance.store.ts
+++ apps/ui/src/app/routes/finance/store/finance.store.ts
@@ -1,5 +1,9 @@
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 import { withUser } from '../../user/store/with-user';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { inject } from '@angular/core';
+import { FinanceHttpService } from './finance-http.service';
 
 type FinanceState = {
   loading: boolean;
@@ -13,6 +17,8 @@ export const FinanceStore = signalStore(
   { providedIn: 'root', protectedState: false },
   withState(initialState),
   withMethods((store) => {
+    const http = inject(FinanceHttpService);
+
     /**
      * Resets the finance store to
      * initial state
@@ -21,7 +27,10 @@ export const FinanceStore = signalStore(
      */
     const _reset = (): void => patchState(store, initialState);
 
+    const fetch = rxMethod<void>(switchMap(() => http.fetch$()));
+
     return {
+      fetch,
       _reset,
     };
   }),
