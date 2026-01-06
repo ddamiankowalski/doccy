f0720597e958afe0d290845737283bddacf14163
Damian <dkowalski.1997@gmail.com>
2026-01-06 17:48:14 +0100

    added global err handler

--- apps/api/error/error-handler.js
+++ apps/api/error/error-handler.js
@@ -5,10 +5,16 @@ const logger = require('../logger/logger');
  * 
  * @param {*} err 
  * @param {*} _ 
- * @param {*} res 
+ * @param {*} res
+ * @param {*} __ 
  */
-const errorHandler = (err, _, res) => {
+const errorHandler = (err, _, res, __) => {
   logger.error(err.message);
+
+  if (err.status === undefined) {
+    err.status = 500;
+  }
+
   res.status(err.status).json({ status: err.status, message: err.message });
 }
 
--- apps/api/server.js
+++ apps/api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("./logger/logger.js");
+
 const { migrate } = require("./database/scripts/db-migrate.js");
+const { errorHandler } = require("./error/error-handler.js");
 
 /**
  * Routes
@@ -13,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api/assets", assets);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   logger.log(`Restarted the server successfully on port ${PORT}`);
