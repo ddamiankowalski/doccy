cd607318fece51738675d05eb9cc60ebb523a925
Damian <dkowalski.1997@gmail.com>
2026-01-06 17:56:43 +0100

    wip

--- apps/ui/src/app/ui/input/input-form/input-form.ts
+++ apps/ui/src/app/ui/input/input-form/input-form.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 export type InputType = 'text' | 'number' | 'select';
 
 export type InputField = {
   id: string;
   type: InputType;
+  required: boolean;
+  label: string;
+  placeholder: string;
 };
 
 @Component({
   selector: 'dc-input-form',
-  template: ``,
+  template: `
+    <form class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] max-w-[calc(2*1fr)]">
+      @for(field of fields(); track field.id) {}
+    </form>
+  `,
 })
 export class InputForm {
-  public;
+  public fields = input.required<InputField[]>();
 }
