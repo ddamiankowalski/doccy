979dedee023914689c960b29f60fdc825c145b00
Damian <dkowalski.1997@gmail.com>
2026-01-09 19:49:52 +0100

    wip

--- apps/api/routes/market-route.js
+++ apps/api/routes/market-route.js
@@ -5,7 +5,7 @@ const Market = require('../controllers/market-controller');
 const SystemError = require('../error/system-error');
 
 router.get('/search', async (req, res) => {
-  const { symbol } = req.params;
+  const { symbol } = req.query;
 
   if (!symbol) {
     throw new SystemError(403, 'Cannot search for symbols without providing value');
