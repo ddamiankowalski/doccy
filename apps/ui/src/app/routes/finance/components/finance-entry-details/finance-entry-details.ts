2d23bba777a18990241f3e1941f5cf29929d7e50
Damian Kowalski <dkowalski.1997@gmail.com>
2026-01-11 00:19:40 +0100

    added table component

--- apps/ui/src/app/routes/finance/components/finance-entry-details/finance-entry-details.ts
+++ apps/ui/src/app/routes/finance/components/finance-entry-details/finance-entry-details.ts
@@ -1,13 +1,13 @@
-import { Component, signal, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { PrimaryButton } from '../../../../ui/button/primary-button/primary-button';
 import { LucideAngularModule } from 'lucide-angular';
 import { SecondaryButton } from '../../../../ui/button/secondary-button/secondary-button';
-import { IconButton } from '../../../../ui/button/icon-button/icon-button';
+import { Table } from '../../../../ui/table/components/table';
 
 @Component({
   selector: 'dc-finance-entry-details',
   encapsulation: ViewEncapsulation.None,
-  imports: [PrimaryButton, SecondaryButton, LucideAngularModule, IconButton],
+  imports: [PrimaryButton, SecondaryButton, LucideAngularModule, Table],
   template: `
     <div class="w-full text-xs">
       <div class="flex gap-2 mb-2">
@@ -15,36 +15,10 @@ import { IconButton } from '../../../../ui/button/icon-button/icon-button';
         <dc-secondary-button icon="plus" [isDisabled]="true"> Remove selected </dc-secondary-button>
       </div>
 
-      <!-- Header (fixed, no scroll) -->
-      <table class="w-full table-fixed ">
-        <thead class="bg-charcoal-light ">
-          <tr class="border-b border-white/5 ">
-            <th class="w-1/3 px-4 py-3 text-left font-semibold text-gray-400 text-xs">Name</th>
-            <th class="w-1/3 px-4 py-3 text-left font-semibold text-gray-400 text-xs">Artist</th>
-            <th class="w-1/3 px-4 py-3 text-left font-semibold text-gray-400 text-xs">Date</th>
-          </tr>
-        </thead>
-      </table>
-
-      i am able
-
-      <div class="flex gap-2 mt-4 items-center justify-center text-gray-400 text-xs font-medium">
-        <dc-icon-button (clicked)="onPreviousClick()" name="chevron-left" />
-        <span>Page {{ currentPage() }} of {{ allPages() }}</span>
-        <dc-icon-button (clicked)="onNextClick()" name="chevron-right" />
-      </div>
+      <dc-table/>
     </div>
   `,
 })
 export class FinanceEntryDetails {
-  public currentPage = signal<number>(0);
-  public allPages = signal<number>(10);
 
-  public onPreviousClick(): void {
-    this.currentPage.update((page) => --page);
-  }
-
-  public onNextClick(): void {
-    this.currentPage.update((page) => ++page);
-  }
 }
--- apps/ui/src/app/ui/table/components/table.ts
+++ apps/ui/src/app/ui/table/components/table.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { IconButton } from "../../button/icon-button/icon-button";
 
 @Component({
   selector: 'dc-table',
+  imports: [IconButton],
   template: `
-  <div class="max-h-[25rem] overflow-y-auto">
+        <table class="w-full table-fixed ">
+        <thead class="bg-charcoal-light ">
+          <tr class="border-b border-white/5 ">
+            <th class="w-1/3 px-4 py-3 text-left font-semibold text-gray-400 text-xs">Name</th>
+            <th class="w-1/3 px-4 py-3 text-left font-semibold text-gray-400 text-xs">Artist</th>
+            <th class="w-1/3 px-4 py-3 text-left font-semibold text-gray-400 text-xs">Date</th>
+          </tr>
+        </thead>
+      </table>
+  <div class="max-h-100 overflow-y-auto">
         <table class="w-full table-fixed text-xs">
           <tbody>
             <tr class="border-b border-white/5 hover:bg-white/5">
@@ -124,6 +135,23 @@ import { Component } from "@angular/core";
             <!-- more rows -->
           </tbody>
         </table>
+      </div>
+
+            <div class="flex gap-2 mt-2 items-center justify-center text-gray-400 text-xs font-medium">
+        <dc-icon-button (clicked)="onPreviousClick()" name="chevron-left" />
+        <span>Page {{ currentPage() }} of {{ allPages() }}</span>
+        <dc-icon-button (clicked)="onNextClick()" name="chevron-right" />
       </div>`
 })
-export class Table {}
+export class Table {
+    public currentPage = signal<number>(0);
+  public allPages = signal<number>(10);
+
+  public onPreviousClick(): void {
+    this.currentPage.update((page) => --page);
+  }
+
+  public onNextClick(): void {
+    this.currentPage.update((page) => ++page);
+  }
+}
