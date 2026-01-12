122528ef515834267437c9272f095b735953c8ef
Damian <dkowalski.1997@gmail.com>
2026-01-13 00:33:06 +0100

    finance tile footer

--- apps/ui/src/app/routes/finance/components/finance-tile/finance-tile-footer/finance-tile-footer.ts
+++ apps/ui/src/app/routes/finance/components/finance-tile/finance-tile-footer/finance-tile-footer.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ProgressBar } from '../../../../../ui/components/progress-bar/progress-bar';
+import { FinanceEntry } from '../../../store/finance.store';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'dc-finance-tile-footer',
-  imports: [ProgressBar],
-  template: ` <dc-progress-bar class="mt-auto" [value]="65" description="House down payment" />`,
+  imports: [ProgressBar, KeyValuePipe],
+  template: ` @for (item of entry() | keyvalue; track item.key) {
+    @if (item.value !== undefined) {
+      @switch (item.key) {
+        @case ('houseDownPayment') {
+          <dc-progress-bar class="mt-auto" [value]="65" description="House down payment" />
+        }
+
+        @case ('retirementFund') {
+          <dc-progress-bar class="mt-auto" [value]="65" description="House down payment" />
+        }
+      }
+    }
+  }`,
 })
-export class FinanceTileFooter {}
+export class FinanceTileFooter {
+  public entry = input.required<FinanceEntry>();
+}
