b741c99a1dddab55c1e797fe9160bb7b6a01362a
Damian Kowalski <dkowalski.1997@gmail.com>
2026-01-18 15:09:56 +0100

    added finance events

---
+++ apps/ui/src/app/routes/finance/store/finance-events.ts
@@ -0,0 +1,13 @@
+import { type } from '@ngrx/signals';
+import { eventGroup } from '@ngrx/signals/events';
+import { SectionName } from './finance.store';
+
+/**
+ * All finance events
+ */
+export const financeEvents = eventGroup({
+  source: 'FinanceStore',
+  events: {
+    recordUpdate: type<{ id: string; section: SectionName }>(),
+  },
+});
